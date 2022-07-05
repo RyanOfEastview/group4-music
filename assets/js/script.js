ebc6c22bcc76a28276d2538d5b3afc2e11f8bc7d
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-07-05 17:12:23 -0400

    created api ticketmaster function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 var apiKey = `7elxdku9GGG5k8j0Xm8KWdANDgecHMV0`;
 
-function () {
-    console.log();
+
+var getDates = function(attraction) {
+    // format URL to search by attraction/band
+    var apiUrl = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0";
+
+    // make a request to the URL
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
+
+getDates();
