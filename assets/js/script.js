359cb58c4a9a3eb62cd89145c652b12a159e61bf
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-07-11 17:41:11 -0400

    cleaned up comments in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ var getDates = function(artist) {
     //  pass response data to DOM
         console.log(data);
 
-        displayDates(data,artist);//missed artist
+        displayDates(data,artist);
       });
     } else {
       alert('Error: Artist Not Found');
@@ -55,44 +55,45 @@ var displayDates = function(dates, searchTerm) {
 
   //check if api returned any tour dates
    if(numOfevents === 0){
-      //  We can direct them to live videos here. Some other function.
+      //  We can direct them to live videos or second API here
       datesContainerEl.textContent = 'No tour dates found.';
       // return;
-  }else{
+    }else{
+    
     for(var i = 0; i < 2; i++){
       var tmEvents = dates._embedded.events[i];
-      console.log(tmEvents);
-      console.log(tmEvents.name);
-      console.log(tmEvents.dates.start.localDate);
-      console.log(tmEvents.dates.start.localTime)
-        
-       
+      // console.log(tmEvents);
+      // console.log(tmEvents.name);
+      // console.log(tmEvents.dates.start.localDate);
+      // console.log(tmEvents.dates.start.localTime)
     }
+
     // loop through tour dates
     for (var i = 0; i < numOfevents; i++) {
       var tmEvents = dates._embedded.events[i];
-      // format date name
-
-      // var date = events[i].venues.name + " - " + dates.start.localDate + " - " + dates.start.localTime;
+    
+      // format date name that is displayed on screen
       var date = tmEvents.name + " - " + tmEvents.dates.start.localDate +
         " - " + tmEvents.dates.start.localTime + " - " + tmEvents._embedded.venues[0].name + " - " + tmEvents._embedded.venues[0].city.name + " - " + tmEvents._embedded.venues[0].country.countryCode;
-
       console.log(date);
+
       //create a link element to take users to a link to buy tickets for chosen date
-            var linkEl = document.createElement("a");
-            linkEl.classList = "list-item flex-row justify-space-between align-center";
-            var newLine = document.createElement("br");
-            //new
-            console.log(tmEvents.url);
-            var ticketLink = document.createTextNode("" + date);
-            linkEl.appendChild(ticketLink);
-            linkEl.title = "";
-            linkEl.href = tmEvents.url;
-            //set for opening a new tab for the link
-            linkEl.target = "_blank";
-            linkEl.rel = "noopener noreferrer";
-            datesContainerEl.appendChild(linkEl);
-            datesContainerEl.appendChild(newLine);
+      var linkEl = document.createElement("a");
+      linkEl.classList = "list-item flex-row justify-space-between align-center";
+      
+      var newLine = document.createElement("br");
+      console.log(tmEvents.url);
+      
+      var ticketLink = document.createTextNode("" + date);
+      linkEl.appendChild(ticketLink);
+      linkEl.title = "";
+      linkEl.href = tmEvents.url;
+      
+      //set for opening a new tab for the link
+      linkEl.target = "_blank";
+      linkEl.rel = "noopener noreferrer";
+      datesContainerEl.appendChild(linkEl);
+      datesContainerEl.appendChild(newLine);
 
     }
   }
