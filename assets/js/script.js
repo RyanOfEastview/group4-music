af9df0d411c0f252759ba64fe833edd279da9a5f
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-07-11 17:35:11 -0400

    Styling coming along. TM API tested and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,22 @@ var artistSearchTerm = document.querySelector("#artist-search-term");
 var datesContainerEl = document.querySelector("#dates-container");
 
 //get value from input element
-
 var formSubmit = function(event) {
-    event.preventDefault();
-    // console.log(event);
-    var artist = nameInputEl.value.trim();
+  event.preventDefault();
+  // console.log(event);
+  var artist = nameInputEl.value.trim();
 
-    if (artist) {
-        getDates(artist);
+  if (artist) {
+    getDates(artist);
 
-       // clear old content
-        searchContainerEl.textContent = "";
-        nameInputEl.value = "";
-    }
-    // }else{
-    //     alert("please enter an artist's name");
-    // }
+    // clear old content
+    searchContainerEl.textContent = "";
+    nameInputEl.value = "";
+  }
 };
 
 var getDates = function(artist) {
-    // format URL to search by attraction/band - IN PROGRESS
+    // format URL to search by attraction/band 
     var apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=` + artist + `&apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0`;
 
     // make a get request to url
@@ -40,7 +36,8 @@ var getDates = function(artist) {
       response.json().then(function(data) {
     //  pass response data to DOM
         console.log(data);
-        displayDates(data);
+
+        displayDates(data,artist);//missed artist
       });
     } else {
       alert('Error: Artist Not Found');
@@ -52,48 +49,53 @@ var getDates = function(artist) {
 };
 
 var displayDates = function(dates, searchTerm) {
-  //check if api returned any tour dates
-  if (dates.length === 0) {
-    //  We can direct them to live videos here. Some other function.
-    datesContainerEl.textContent = 'No tour dates found.';
-    return;
-  }
-
   artistSearchTerm.textContent = searchTerm;
-  
-  // loop through tour dates
-  for (var i = 0; i < dates.length; i++) {
-    // format date name
-    var date = events[i].venues.name + " - " + dates.start.localDate + " - " + dates.start.localTime;
-    
-    //create a link element to take users to a link to buy tickets for chosen date
-    var dateEl = document.createElement("a");
-    dateEl.classList = "list-item flex-row justify-space-between align-center";
 
-    dateEl.setAttribute//link to ticketmaster tickets here
+  var numOfevents = dates.page.totalElements;
 
-    // dateEl.setAttribute("href", dates[i].html_url);
-    // dateEl.setAttribute("target", "_blank");
+  //check if api returned any tour dates
+   if(numOfevents === 0){
+      //  We can direct them to live videos here. Some other function.
+      datesContainerEl.textContent = 'No tour dates found.';
+      // return;
+  }else{
+    for(var i = 0; i < 2; i++){
+      var tmEvents = dates._embedded.events[i];
+      console.log(tmEvents);
+      console.log(tmEvents.name);
+      console.log(tmEvents.dates.start.localDate);
+      console.log(tmEvents.dates.start.localTime)
+        
+       
+    }
+    // loop through tour dates
+    for (var i = 0; i < numOfevents; i++) {
+      var tmEvents = dates._embedded.events[i];
+      // format date name
 
-    //create span to hold date element
-    var titleEl = document.createElement("span");
-    titleEl.textContent = date;
+      // var date = events[i].venues.name + " - " + dates.start.localDate + " - " + dates.start.localTime;
+      var date = tmEvents.name + " - " + tmEvents.dates.start.localDate +
+        " - " + tmEvents.dates.start.localTime + " - " + tmEvents._embedded.venues[0].name + " - " + tmEvents._embedded.venues[0].city.name + " - " + tmEvents._embedded.venues[0].country.countryCode;
 
-    // append to container
-    dateEl.appendChild(titleEl)
+      console.log(date);
+      //create a link element to take users to a link to buy tickets for chosen date
+            var linkEl = document.createElement("a");
+            linkEl.classList = "list-item flex-row justify-space-between align-center";
+            var newLine = document.createElement("br");
+            //new
+            console.log(tmEvents.url);
+            var ticketLink = document.createTextNode("" + date);
+            linkEl.appendChild(ticketLink);
+            linkEl.title = "";
+            linkEl.href = tmEvents.url;
+            //set for opening a new tab for the link
+            linkEl.target = "_blank";
+            linkEl.rel = "noopener noreferrer";
+            datesContainerEl.appendChild(linkEl);
+            datesContainerEl.appendChild(newLine);
 
-    // create a type "status" element
-    var typeEl = document.createElement("span");
-    dateEl.classList = "flex-row align-center";
-
-    // check other details here if required
-
-    // append to container
-    dateEl.appendChild(typeEl);
-
-
+    }
   }
-  
-  };
+};
 
 artistFormEl.addEventListener("submit", formSubmit);
